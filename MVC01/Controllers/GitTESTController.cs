39b4f9cb7885d466fa0358ad59ee02273e6b952e
dminer0067 <dminer0067@gmail.com>
2022-01-28 14:54:44 +0530

    Commit to Git

---
+++ MVC01/Controllers/GitTESTController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC01.Controllers
+{
+    public class GitTESTController : Controller
+    {
+        // GET: GitTEST
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
