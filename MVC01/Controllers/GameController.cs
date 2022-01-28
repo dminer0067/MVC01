f53b9f1a56fd3e6856360bb9c6273df31211e1a6
NIRMALPC <NIRMALPC@NIRMALDELL5410>
2022-01-28 14:35:20 +0530

    Add project files.

---
+++ MVC01/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC01.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public string Index()
+        {
+            return "This is Game Controlle.";
+        }
+
+        public string Names(string fname, string lname)
+        {
+            return "Firstname : " + fname +" "+ "Lastname : " + lname;
+        }
+    }
+}
